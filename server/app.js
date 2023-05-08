4da1276f03849e3df7d3664f3a2f23e7cb649a28
kartikDeveloper1 <98469991+kartikDeveloper1@users.noreply.github.com>
2023-05-08 14:09:34 +0530

    updated now2

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json()) //We need to tell our application to accept json data fo
 
 app.use(require("./router/auth"))
 
-const PORT=process.env.PORT 
+const PORT=process.env.PORT || 5000 
 
 /*
 Routes
